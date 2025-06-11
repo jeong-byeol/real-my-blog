7a8621f43ab45b7fe2e36f3405673f3af40a6315
jeong-byeol <wjdgksquf2021@naver.com>
2025-06-11 16:22:27 +0900

    ㅇㄹ

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename="/real-my-blog">
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage posts={posts} />} />
